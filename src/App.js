74a53e4678fc97149b189455cdc0779773d3f5f5
nidarprog <ahmedismael12@gmail.com>
2023-07-03 12:39:53 +0200

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import Fuse from 'fuse.js';
+import data from './data.json';
 import './App.css';
 
 function App() {
+  const { t, i18n } = useTranslation();
+  const [searchTerm1, setSearchTerm1] = useState('');
+  const [searchTerm2, setSearchTerm2] = useState('');
+  const [swapped, setSwapped] = useState(false);
+
+  const fuse = new Fuse(Object.keys(data), { includeScore: true });
+
+  const handleLanguageChange = (language) => {
+    i18n.changeLanguage(language);
+  };
+
+  const handleSearch1 = (e) => {
+    const term = e.target.value;
+    setSearchTerm1(term);
+    setSearchTerm2(term);
+  };
+
+  const handleSearch2 = (e) => {
+    const term = e.target.value;
+    setSearchTerm1(term);
+    setSearchTerm2(term);
+  };
+
+  const handleSwap = () => {
+    setSwapped(!swapped);
+  };
+
+  const getTranslation = (key) => {
+    const translation = t(key);
+    return swapped ? swapWords(translation) : translation;
+  };
+
+  const swapWords = (text) => {
+    const words = text.split(' ');
+    return words
+      .map((word, index) => {
+        return index % 2 === 0 ? word.toUpperCase() : word.toLowerCase();
+      })
+      .join(' ');
+  };
+
+  const searchResults1 = searchTerm1
+    ? fuse.search(searchTerm1).map((result) => result.item)
+    : Object.keys(data);
+
+  const searchResults2 = searchTerm2
+    ? fuse.search(searchTerm2).map((result) => result.item)
+    : Object.keys(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>
+        {t('hello')} {t('world')}!
+      </h1>
+      <div className="language-buttons">
+        <button onClick={() => handleLanguageChange('en')}>English</button>
+        <button onClick={() => handleLanguageChange('es')}>Español</button>
+        <button onClick={() => handleLanguageChange('fr')}>Français</button>
+      </div>
+      <div className="search-container">
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="Search 1"
+            className="search-input"
+            value={searchTerm1}
+            onChange={handleSearch1}
+          />
+        </div>
+        <div className="swap-container">
+          <button className="swap-button" onClick={handleSwap}>
+            Swap
+          </button>
+        </div>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="Search 2"
+            className="search-input"
+            value={searchTerm2}
+            onChange={handleSearch2}
+          />
+        </div>
+      </div>
+      <div className="results-container">
+        <ul>
+          {searchResults1.map((key) => (
+            <li key={key}>{getTranslation(key)}</li>
+          ))}
+        </ul>
+        <ul>
+          {searchResults2.map((key) => (
+            <li key={key}>{getTranslation(key)}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import i18n from 'i18next';
+import { I18nextProvider } from 'react-i18next';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+i18n.init({
+  interpolation: { escapeValue: false }, // React already does escaping
+  lng: 'en', // default language
+  resources: {
+    en: {
+      translation: require('./translations/en.json')
+    },
+    es: {
+      translation: require('./translations/es.json')
+    },
+    fr: {
+      translation: require('./translations/fr.json')
+    }
+  }
+});
+
+ReactDOM.render(
+  <I18nextProvider i18n={i18n}>
     <App />
-  </React.StrictMode>
+  </I18nextProvider>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
