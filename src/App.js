c2eb2a37ee74879e6ef64b8f7bfa80cae56dc8c6
mohamed-79 <mohamederedni4@gmail.com>
2023-07-12 18:00:47 +0300

    heta gohrin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import translations from './translations/wergeran.json';
+import  Button  from 'react-bootstrap/Button';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [currentLanguage, setCurrentLanguage] = useState('en');
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -14,41 +16,67 @@ function App() {
   };
 
   const searchInTranslations = (term) => {
-    const matchingWords = Object.keys(translations).filter((word) =>
-      word.toLowerCase().includes(term.toLowerCase()) ||
-      translateWord(word, 'en').toLowerCase().includes(term.toLowerCase()) ||
-      translateWord(word,  'ku').toLowerCase().includes(term.toLowerCase())
+    const matchingWords = Object.keys(translations).filter(
+      (word) =>
+        word.toLowerCase().includes(term.toLowerCase()) ||
+        translateWord(word, 'en').toLowerCase().includes(term.toLowerCase()) ||
+        translateWord(word, 'ku').toLowerCase().includes(term.toLowerCase())
     );
     return matchingWords;
   };
 
   const filteredWords = searchInTranslations(searchTerm);
 
+  const toggleLanguage = () => {
+    setCurrentLanguage((prevLanguage) => (prevLanguage === 'en' ? 'ku' : 'en'));
+  };
+
+  if (searchTerm === '') {
+    return (
+      <div>
+       
+        <h1>بەرنامێ وەرگێڕانا پەیڤا</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="...لێگەریانا پەیڤا"
+            value={searchTerm}
+            onChange={handleSearch}
+          />
+          <p>زاراڤەکی داخل بکە بو وەرگێڕانێ</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div>
+    <div> 
+    
       <h1>Word Translation App</h1>
       <div>
+    
+
         <input
           type="text"
           placeholder="Search for a word..."
           value={searchTerm}
           onChange={handleSearch}
         />
+        <Button variant="dark" onClick={toggleLanguage}>
+           {currentLanguage === 'en' ? 'کوردی' : 'English'}
+        </Button>        
+    
+        
+        {filteredWords.length === 0 && <p>No matching words found.</p>}
         {filteredWords.map((word) => (
           <div key={word}>
-            
             <p>
-              <strong>English: </strong>
-              {translateWord(word, 'en')}
+              <strong>{currentLanguage === 'en' ? 'English' : 'کوردی'}: </strong>
+              {translateWord(word, currentLanguage)}
             </p>
-            <div >
-              <strong>Kurdish: </strong>
-              {translateWord(word, 'ku')}
-            </div>
-
           </div>
         ))}
-        {filteredWords.length === 0 && <p>No matching words found.</p>}
+
       </div>
     </div>
   );
